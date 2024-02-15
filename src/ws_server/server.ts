75aefdd226607a5642d09e7fd48062681da20989
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-15 22:16:05 +0300

    feat: create ws server, add events

---
+++ src/ws_server/server.ts
@@ -0,0 +1,18 @@
+import { WebSocketServer } from 'ws';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 8181;
+console.log(typeof PORT)
+const wsServer = new WebSocketServer({ port: Number(PORT) })
+wsServer.on('listening', () => {
+  console.log(`WS Server is starting on the ${PORT} `);
+  console.log(`WebSocket server parameters: ${JSON.stringify(wsServer.options)}`);
+})
+
+wsServer.on('connection', (ws) => {
+  console.log(`New WebSocket connection`);
+  ws.on('message', (message) => {
+    console.log(message);
+  })
+});
